9e2384532340d474568fcb3c4399e49e4c07412b
ArDezZz <aromudez@gmail.com>
2022-08-18 13:50:58 +0300

    search-page-classes

--- src/components/Search-page/SearchPage.jsx
+++ src/components/Search-page/SearchPage.jsx
@@ -3,13 +3,21 @@ import React from 'react';
 const SearchPage = () => {
   return (
     <div className="search-page">
-      <h2 className="search-page__title">SEARCH FLIGHT</h2>
-      <div className="search-page__field">
-        <span className="search-page__loop"></span>
-        <form className="search-page__form">
-          <input type="text" placeholder="Airline, destination or flight #" />
-          <button type="submit">Search</button>
-        </form>
+      <div className="search-page-container">
+        <h2 className="search-page__title">SEARCH FLIGHT</h2>
+        <div className="search-field">
+          <span className="search-field__loop"></span>
+          <form className="search-field__form">
+            <input
+              className="search-field__input"
+              type="text"
+              placeholder="Airline, destination or flight #"
+            />
+            <button className="search-field__btn" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
