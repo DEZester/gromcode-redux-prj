b5083711c40cdbf692873c51524fff4f14eebb06
ArDezZz <aromudez@gmail.com>
2022-08-18 12:44:04 +0300

    skelet of the search-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Hello, React!</div>;
+  return (
+    <div className="page">
+      <div className="search-page">
+        <h2 className="search-page__title">SEARCH FLIGHT</h2>
+        <div className="search-page__field">
+          <span className="search-page__loop"></span>
+          <form className="search-page__form">
+            <input type="text" placeholder="Airline, destination or flight #" />
+            <button type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
