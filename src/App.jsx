e1a3c17a72fa5641992070cba6737bf4e5f8c581
ArDezZz <aromudez@gmail.com>
2022-08-22 17:51:57 +0300

    flights page layout links

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import SearchPage from './components/Search-page/SearchPage';
+import SearchPage from './components/SearchPage/SearchPage';
 
 const App = () => {
   return (
     <div className="page">
       <SearchPage />
+      <div className="flights">
+        <div className="flights-container">
+          <a href="#" className="flights__link">
+            departures
+          </a>
+          <a href="#" className="flights__link flights__link__active">
+            arrivals
+          </a>
+        </div>
+      </div>
+      <div className="flights_dates">
+        <input type="date" className="dates__choise" />
+        <div className="dates__day">
+          <span className="dates__day-number">21/08</span>
+          <p>SUNDAY</p>
+        </div>
+        <div className="dates__day">
+          <span className="dates__day-number">21/08</span>
+          <p>SUNDAY</p>
+        </div>
+        <div className="dates__day">
+          <span className="dates__day-number">21/08</span>
+          <p>SUNDAY</p>
+        </div>
+      </div>
     </div>
   );
 };
