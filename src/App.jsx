39275bd6e085edfcad49b5fd9e9a92d22d1ab0ae
ArDezZz <aromudez@gmail.com>
2022-08-22 18:33:30 +0300

    flightspage dates

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,28 @@ const App = () => {
     <div className="page">
       <SearchPage />
       <div className="flights">
-        <div className="flights-container">
-          <a href="#" className="flights__link">
+        <div className="flights__links">
+          <a href="#" className="flights__links-item">
             departures
           </a>
-          <a href="#" className="flights__link flights__link__active">
+          <a href="#" className="flights__links-item flights__link-active">
             arrivals
           </a>
         </div>
-      </div>
-      <div className="flights_dates">
-        <input type="date" className="dates__choise" />
-        <div className="dates__day">
-          <span className="dates__day-number">21/08</span>
-          <p>SUNDAY</p>
-        </div>
-        <div className="dates__day">
-          <span className="dates__day-number">21/08</span>
-          <p>SUNDAY</p>
-        </div>
-        <div className="dates__day">
-          <span className="dates__day-number">21/08</span>
-          <p>SUNDAY</p>
+        <div className="flights__dates dates">
+          <input type="date" className="dates__choise" />
+          <div className="dates__day">
+            <span className="dates__day-number">21/08</span>
+            <p>SUNDAY</p>
+          </div>
+          <div className="dates__day">
+            <span className="dates__day-number">21/08</span>
+            <p>SUNDAY</p>
+          </div>
+          <div className="dates__day dates__day__active">
+            <span className="dates__day-number">21/08</span>
+            <p>SUNDAY</p>
+          </div>
         </div>
       </div>
     </div>
