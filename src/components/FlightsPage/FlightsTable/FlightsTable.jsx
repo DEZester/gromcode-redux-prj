1059ed4241f03fd5e3970765478e53a9bae80ffa
ArDezZz <aromudez@gmail.com>
2022-08-23 14:23:03 +0300

    FlightsTableItem component

--- src/components/FlightsPage/FlightsTable/FlightsTable.jsx
+++ src/components/FlightsPage/FlightsTable/FlightsTable.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FlightsTableItem from './FlightsTableItem';
 
 const FlightsTable = () => {
   return (
@@ -14,29 +15,10 @@ const FlightsTable = () => {
         </tr>
       </thead>
       <tbody className="table__list">
-        <tr className="table__raw">
-          <th className="table__item">
-            <span className="table__item-terminal table__item-terminal__d">D</span>
-          </th>
-          <th className="table__item">9:05</th>
-          <th className="table__item">London</th>
-          <th className="table__item">DP</th>
-          <th className="table__item-airline">
-            <ul className="table__item-airline-list">
-              <li>
-                <img
-                  className="table__item-logo"
-                  src="https://api.iev.aero/media/airline/files/606aefa0c8a4a734421442.png"
-                  alt="logo"
-                />
-              </li>
-              <li>
-                <span>Bees Airline</span>
-              </li>
-            </ul>
-          </th>
-          <th className="table__item">7B2001</th>
-        </tr>
+        <FlightsTableItem />
+        <FlightsTableItem />
+        <FlightsTableItem />
+        <FlightsTableItem />
       </tbody>
     </table>
   );
