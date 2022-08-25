a69ca8e30a763ac5b5e7a1f6b9514d8df9fcd183
ArDezZz <aromudez@gmail.com>
2022-08-25 18:53:21 +0300

    adding noFlights

--- src/components/FlightsPage/FlightsLinks/FlightsLink.jsx
+++ src/components/FlightsPage/FlightsLinks/FlightsLink.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const FlightsLink = ({ headerName }) => {
+const FlightsLink = ({ headerName, changeStatus }) => {
+  const setStatus = () => {
+    if (headerName === 'departures') {
+      changeStatus(true);
+    }
+    if (headerName === 'arrivals') {
+      changeStatus(false);
+    }
+  };
   return (
     <NavLink
       to={headerName}
       className={({ isActive }) => (isActive ? `links__item links__item-active` : `links__item`)}
+      onClick={setStatus}
     >
       {headerName}
     </NavLink>
--- src/components/FlightsPage/FlightsPage.jsx
+++ src/components/FlightsPage/FlightsPage.jsx
@@ -4,29 +4,28 @@ import { connect } from 'react-redux';
 import FlightsDates from './FlightsDates/FlightsDates';
 import FlightsLink from './FlightsLinks/FlightsLink';
 import FlightsTable from './FlightsTable/FlightsTable';
+import NoFlights from './NoFlights/NoFights';
 
 import { departuresSelector, arrivalsSelector } from './features/flights.selectors';
 import * as flightsActions from './features/flights.actions';
 
 const FlightsPage = ({ departureFlightsList, arrivalsFlightsList, getFlightsList }) => {
-  const [isActive, setStatusActive] = useState(true);
+  const [isActive, changeStatus] = useState(true);
 
   useEffect(() => {
     getFlightsList();
   }, []);
 
+  const flightsList = isActive ? departureFlightsList : arrivalsFlightsList;
+
   return (
     <div className="flights">
       <div className="links">
-        <FlightsLink headerName="departures" />
-        <FlightsLink headerName="arrivals" />
+        <FlightsLink headerName="departures" changeStatus={changeStatus} />
+        <FlightsLink headerName="arrivals" changeStatus={changeStatus} />
       </div>
       <FlightsDates />
-      {isActive ? (
-        <FlightsTable flightsList={departureFlightsList} />
-      ) : (
-        <FlightsTable flightsList={arrivalsFlightsList} />
-      )}
+      {flightsList.length > 0 ? <FlightsTable flightsList={flightsList} /> : <NoFlights />}
     </div>
   );
 };
