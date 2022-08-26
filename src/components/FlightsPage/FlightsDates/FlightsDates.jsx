19743227c315cd0786c417891f4f96d67b45e15b
ArDezZz <aromudez@gmail.com>
2022-08-26 18:28:46 +0300

    yesterday today tommorow in FlightsDates

--- src/components/FlightsPage/FlightsDates/FlightsDates.jsx
+++ src/components/FlightsPage/FlightsDates/FlightsDates.jsx
@@ -5,21 +5,32 @@ const FlightsDates = ({ changeDate }) => {
   const changeDateHandler = e => {
     changeDate(moment(e.target.value).format('DD-MM-yy'));
   };
+  const currentDate = new Date();
+
+  const tomorrow = date => {
+    let tomorrow = new Date();
+    return moment(tomorrow.setDate(date.getDate() + 1)).format('DD/MM');
+  };
+
+  const yesterday = date => {
+    let yesterday = new Date();
+    return moment(yesterday.setDate(date.getDate() - 1)).format('DD/MM');
+  };
 
   return (
     <div className="dates">
       <input type="date" className="dates__choise" onChange={changeDateHandler} />
       <div className="dates__day">
-        <span className="dates__day-number">21/08</span>
-        <p>SUNDAY</p>
+        <span className="dates__day-number">{yesterday(currentDate)}</span>
+        <p>YESTERDAY</p>
       </div>
       <div className="dates__day">
-        <span className="dates__day-number">21/08</span>
-        <p>SUNDAY</p>
+        <span className="dates__day-number">{moment(currentDate).format('DD/MM')}</span>
+        <p>TODAY</p>
       </div>
       <div className="dates__day dates__day__active">
-        <span className="dates__day-number">21/08</span>
-        <p>SUNDAY</p>
+        <span className="dates__day-number">{tomorrow(currentDate)}</span>
+        <p>TOMORROW</p>
       </div>
     </div>
   );
