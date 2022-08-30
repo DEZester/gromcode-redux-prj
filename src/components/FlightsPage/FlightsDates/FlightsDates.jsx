8e5fc3fa7d674b92f9307775e5656ce848cbea41
ArDezZz <aromudez@gmail.com>
2022-08-30 17:40:53 +0300

    yesterday tomorrow today buttons

--- src/components/FlightsPage/FlightsDates/FlightsDates.jsx
+++ src/components/FlightsPage/FlightsDates/FlightsDates.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 import { useLocation, useNavigate } from 'react-router-dom';
+import FlightsChoosenDay from './FlightsChoosenDay';
 const qs = require('qs');
 
 const FlightsDates = () => {
+  const [currentDay, setCurrentDay] = useState();
   const { search } = useLocation();
   const { value } = qs.parse(search.replace('?', ''));
   const navigate = useNavigate();
 
   const changeDateHandler = date => {
     const formatedDate = moment(date).format('DD-MM-YYYY');
+    setCurrentDay(formatedDate);
     const params = qs.stringify({ date: formatedDate, value });
     navigate(`/departures?${params}`);
   };
 
-  const yesterday = moment().subtract(1, 'd').format('DD/MM');
-  const today = moment().format('DD/MM');
-  const tomorrow = moment().add(1, 'd').format('DD/MM');
+  const yesterday = moment().subtract(1, 'd');
+  const today = moment();
+  const tomorrow = moment().add(1, 'd');
 
   return (
     <div className="dates">
@@ -25,18 +28,27 @@ const FlightsDates = () => {
         className="dates__choise"
         onInput={event => changeDateHandler(event.target.value)}
       />
-      <div className="dates__day">
-        <span className="dates__day-number">{yesterday}</span>
-        <p>YESTERDAY</p>
-      </div>
-      <div className="dates__day">
-        <span className="dates__day-number">{today}</span>
-        <p>TODAY</p>
-      </div>
-      <div className="dates__day dates__day__active">
-        <span className="dates__day-number">{tomorrow}</span>
-        <p>TOMORROW</p>
-      </div>
+      <FlightsChoosenDay
+        key={yesterday.format('DDDD')}
+        choosenDay="yesterday"
+        date={yesterday}
+        changeDateHandler={changeDateHandler}
+        currentDay={currentDay}
+      />
+      <FlightsChoosenDay
+        key={today.format('DDDD')}
+        choosenDay="today"
+        date={today}
+        changeDateHandler={changeDateHandler}
+        currentDay={currentDay}
+      />
+      <FlightsChoosenDay
+        key={tomorrow.format('DDDD')}
+        choosenDay="tomorrow"
+        date={tomorrow}
+        changeDateHandler={changeDateHandler}
+        currentDay={currentDay}
+      />
     </div>
   );
 };
